053ddc9f118d8373662f62af99de7ac65bc7ef14
Natalya <natalyamyunster@yahoo.com>
2019-08-16 06:51:19 +0200

    add Strings file

---
+++ src/Strings.java
@@ -0,0 +1,13 @@
+public class Strings
+{
+    public static void main(String[] args)
+    {
+        int x = 5;
+        String s = "Hello";
+        String spase = " ";
+        String name = "Bob";
+        System.out.println(s+spase+name);
+        System.out.println("Hello"+" "+"Jhon");
+        System.out.println("My number is "+x);
+    }
+}
